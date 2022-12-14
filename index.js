00be87d0435cd0b12f5e0f939fdd2642598af935
Angelika <szczepanczykangelika@gmail.com>
2022-12-14 21:28:09 +0100

    update: filtering request handle

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ server.get("/perfumes/filter", (req, res) => {
         : perfume
     );
 
-  res.status(200).jsonp(output.slice((page - 1) * limit, page * limit));
+  res.status(200).json(output.slice((page - 1) * limit, page * limit));
 });
 
 server.use(router);
