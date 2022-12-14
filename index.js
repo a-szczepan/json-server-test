3850872973e92524c2df17e9202de80ed1099798
Angelika <szczepanczykangelika@gmail.com>
2022-12-14 20:53:28 +0100

    update: filtering request handle

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('./src/db.json'); 
+const router = jsonServer.router("./src/db.json");
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
 server.use(middlewares);
+
+server.get("/perfumes/filter", (req, res) => {
+  const db = router.db.toJSON();
+  const page = req.query.page ? req.query.page : 1;
+  const limit = req.query.limit ? req.query.limit : 20;
+  const gender = req.query.gender ? req.query.gender : [];
+  const brand = req.query.brand ? req.query.brand : [];
+  const group = req.query.group ? req.query.group : [];
+
+  let output = db.perfumes.filter(
+    (perfume) =>
+      (brand.length > 0 ? brand.includes(perfume.brand) : perfume) &&
+      (gender.length > 0 ? gender.includes(perfume.gender) : perfume)
+  );
+
+  output = db.perfumes
+    .map((perfume) => {
+      return {
+        ...perfume,
+        mainAccords: perfume.mainAccords.map((e) => e.accord),
+      };
+    })
+    .filter((perfume) =>
+      group.length > 0
+        ? perfume.mainAccords.some((i) => group.includes(i))
+        : perfume
+    );
+
+  res.status(200).jsonp(output.slice((page - 1) * limit, page * limit));
+});
+
 server.use(router);
 
 server.listen(port);
